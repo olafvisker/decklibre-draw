226c0cb8157993c4502f2935efcd681e29a2547c
Olaf Visker <olafvisker@gmail.com>
2025-11-07 14:44:15 +0100

    removed dist added prepare

--- dist/core/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './draw-controller';
-export * from './draw-store';
-export * from './draw-mode';
-export * from './shape-generators';
-export * from './modes';
--- dist/core/modes/draw-point-mode.d.ts
+++
@@ -1,6 +0,0 @@
-import { DrawInfo, DrawMode } from '../draw-mode';
-import { DrawController } from '../draw-controller';
-export declare class DrawPointMode implements DrawMode {
-    onEnter(draw: DrawController): void;
-    onClick(info: DrawInfo, draw: DrawController): void;
-}
